aaaa5d67e965eabc8fa7ef708fd0ac1dfd1dbadf
craineland <craine@disroot.org>
2025-03-24 19:18:03 -0400

    Initializing the project for RockTableFire.org

---
+++ src/main/java/org/rocktablefire/rocktablefire_web/RocktablefireWebApplication.java
@@ -0,0 +1,13 @@
+package org.rocktablefire.rocktablefire_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocktablefireWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocktablefireWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/rocktablefire/rocktablefire_web/model/RSVP.java
@@ -0,0 +1,5 @@
+package org.rocktablefire.rocktablefire_web.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
